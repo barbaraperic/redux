3ed0cc045664c6bb9729fc26803487fc57e55df8
Barbara Peric <b.peric10@gmail.com>
2021-08-10 10:24:59 +0100

    add action objects

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
+const addTodo = { 
+  type: 'ADD_TODO',
+  todo: {
+    id: 0,
+    name: 'Learn Redux',
+    completed: false
+  }
+}
+
+const removeTodo = {
+  type: 'REMOVE_TODO',
+  id: 0
+}
+
+const toggleTodo = {
+  type: 'TOGGLE_TODO',
+  id: 0
+}
+
+const addGoal = {
+  type: 'ADD_GOAL',
+  goal: {
+    id: 0,
+    name: 'Run a marathon'
+  }
+}
+
+const removeGoal = {
+  type: 'REMOVE_GOAL',
+  id: 0,
+}
+
 function createStore() {
   // the store
   // 1. state
