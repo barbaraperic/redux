f11b3bbb922b760de27ba2bcd7301536a2badd7e
Barbara Peric <b.peric10@gmail.com>
2021-08-13 15:46:07 +0100

    goal reducer

--- index.js
+++ index.js
@@ -45,6 +45,15 @@ function todo(state=[], action) {
   }
 }
 
+function goals(state=[], action) {
+  switch(action.type) {
+    case 'ADD_GOAL':
+      return state.concat([action.goal])
+    case 'REMOVE_GOAL':
+      return state.filter((todo) => todo.id !== action.id)
+  }
+}
+
 
 function createStore(reducer) {
   // the store
